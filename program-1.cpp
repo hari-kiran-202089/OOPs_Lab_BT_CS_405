4320d5518679be1a707c09a2cf27342ffe39679e
HariKiran <harikiran.kante@gmail.com>
2022-03-03 16:52:49 +0530

    added user input to program-1

--- program-1.cpp
+++ program-1.cpp
@@ -2,46 +2,59 @@
 #include <iostream>
 using namespace std;
 
-class Matrix{
-    private:
-        int rows=0;
-        int columns=0;
-        int ** matrix = NULL;
-    public:
-        Matrix(int row,int col){
-            rows = row;
-            columns = col;
-            matrix = new int*[rows];
-            for(int i = 0;i<rows;i++){
-                matrix[i] = new int[columns];
-            }
-        }
-        void take_input(){
-            for(int i=0;i<rows;i++){
-                for(int j=0;j<columns;j++){
-                    cout<<"Element["<<i<<','<<j<<"]: ";
-                    cin>>matrix[i][j];
-                }
-            }
-        }
-        void display(){
-            for(int i=0;i<rows;i++){
-                for(int j=0;j<columns;j++){
-                    cout<<matrix[i][j]<<" ";
-                }
-                cout<<endl;
-            }
-        }
-};
+class Matrix
+{
+private:
+    int rows = 0;
+    int columns = 0;
+    int **matrix = NULL;
 
+public:
+    Matrix(int row, int col)
+    {
+        rows = row;
+        columns = col;
+        matrix = new int *[rows];
+        for (int i = 0; i < rows; i++)
+        {
+            matrix[i] = new int[columns];
+        }
+    }
+    void take_input()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                cout << "Element[" << i << ',' << j << "]: ";
+                cin >> matrix[i][j];
+            }
+        }
+    }
+    void display()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                cout << matrix[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
 
 int main()
 {
-    Matrix m1(3,4);
+    int rows, columns;
+    cout << "Enter Rows: ";
+    cin >> rows;
+    cout << "Enter Columns: ";
+    cin >> columns;
+    Matrix m1(rows, columns);
     m1.take_input();
+    cout << "Matrix is: \n";
     m1.display();
-    
+
     return 0;
 }
-
-
