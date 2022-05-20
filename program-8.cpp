49c5c1ba10e28b722f78ac95f1b5d933a49afdec
HariKiran <harikiran.kante@gmail.com>
2022-05-20 15:25:37 +0530

    made program 8 without friend function

--- program-8.cpp
+++ program-8.cpp
@@ -1,88 +1,91 @@
-// Create two classes DM and DB. DM stores the distance in meters and centimeters and DB stores
-// the distance in feet and inches. write a program to add object of DM with object of DB classe
-
 #include <iostream>
-
-using namespace std;
 class DB;
 class DM
 {
 private:
-    double meters = 9; // 1mtr = 100 cm
-    double centimeters = 0;
+    double mtrs;
+    double centmtrs;
 
 public:
-    DM(double mtrs, double cntmtrs)
+    DM(double m, double cm) : mtrs(m), centmtrs(cm)
     {
-        setMeters(mtrs);
-        setCentimeters(cntmtrs);
     }
-    double getMeters() { return meters; }
-    double getCentimeters() { return centimeters; }
-    void setMeters(double mtrs) { meters = mtrs; }
-    void setCentimeters(double centmtrs)
+    double getMtrs()
     {
-        centimeters = centmtrs;
-        meters = meters + (int)centimeters / 100;
-        centimeters = (int)centimeters % 100;
+        return mtrs;
     }
+    double getCentMtrs()
+    {
+        return centmtrs;
+    }
+    void setMtrs(double m)
+    {
+        mtrs = m;
+    }
+    void setCentMtrs(double cm)
+    {
+        centmtrs = cm;
+    }
+    DM add(DB obj);
     void display()
     {
-        cout << meters << "mtrs, " << centimeters << "cms\n";
+        std::cout << "Meters:" << mtrs << " CentMeters:" << centmtrs << std::endl;
     }
-    friend DM add(DM &dm, DB &db);
 };
-
 class DB
 {
 private:
-    double feet = 0; // 1ft = 12 inch
-    double inches = 0;
+    double feet;
+    double inches;
 
 public:
-    DB(double ft, double inch)
+    DB(double ft, double inch) : feet(ft), inches(inch)
     {
-        setFeet(ft);
-        setInches(inch);
     }
-    double getFeet() { return feet; }
-    double getInches() { return inches; }
-    void setFeet(double ft) { feet = ft; }
+    double getFeet()
+    {
+        return feet;
+    }
+    double getInches()
+    {
+        return inches;
+    }
+    void setFeet(double ft)
+    {
+        feet = ft;
+    }
     void setInches(double inch)
     {
         inches = inch;
-        feet = feet + (int)inches / 12;
-        inches = (int)inches % 12;
     }
-    friend DM add(DM &dm, DB &db);
     void display()
     {
-        cout << feet << "ft, " << inches << "inches\n";
+        std::cout << "Feet:" << feet << " Inches:" << inches << std::endl;
     }
 };
-
-DM add(DM &dm, DB &db)
+// Function to add DM object with DB object
+DM DM::add(DB obj)
 {
-    double newCM = db.inches * 2.54;
-    newCM += dm.centimeters;
-    double newMtrs = db.feet * 0.3048;
-    newMtrs += dm.meters;
-    return DM(newMtrs, newCM);
+    double CM = obj.getInches() * 2.54;
+    CM += centmtrs;
+    double MTRS = obj.getFeet() * 0.3048;
+    MTRS += mtrs;
+    return DM(MTRS, CM);
 }
-
 int main()
 {
     double mtrs, centmtrs;
     double feet, inches;
-    cout << "Enter Meters and Centimeters: " << endl;
-    cin >> mtrs >> centmtrs;
-    cout << "Enter Feet and Inches: " << endl;
-    cin >> feet >> inches;
-    DM dm1 = {mtrs, centmtrs};
-    DB db1 = {feet, inches};
-    DM dm2 = add(dm1, db1);
+    std::cout << "Enter Meters and Centimeters: " << std::endl;
+    std::cin >> mtrs >> centmtrs;
+    std::cout << "Enter Feet and Inches: " << std::endl;
+    std::cin >> feet >> inches;
+    DM dm1 = {mtrs, centmtrs}; // Creating object of type DM
+    DB db1 = {feet, inches};   // Creating object of type DB
+    DM dm2 = dm1.add(db1);     // adding DM with DB
     dm1.display();
     db1.display();
+    std::cout << "\nAfter adding DM object with DB object: " << std::endl;
     dm2.display();
 
     return 0;
