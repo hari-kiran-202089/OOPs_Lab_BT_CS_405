a338b69a0410538d354883abef6e5ae93cbe0a21
HariKiran <harikiran.kante@gmail.com>
2022-05-21 01:33:32 +0530

    added program-21

---
+++ program-21.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+T min(T *arr, int len)
+{
+    T min = arr[0];
+    for (int i = 1; i < len; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+int main()
+{
+    int len;
+    cout << "Enter Length of array: ";
+    cin >> len;
+    int *arr = new int(len);
+    for (int i = 0; i < len; i++)
+    {
+        cout << "Element " << i << ": ";
+        cin >> arr[i];
+    }
+    cout << "Min Value: " << min(arr, len);
+    delete[] arr;
+    return 0;
+}
